9e2fba92d6e83442b48f58c5a08945e808796626
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-08-19 14:56:58 -0400

    connection.JS set up tp connect to dog_blog DB

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+//connect to dog_blog db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localthost',
+    dialect: "mysql",
+    port: 3301,
+  });
+
+module.exports = sequelize;
